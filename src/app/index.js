2aacdb63f4978a9a4d3d60647d195b2e1bfed254
furdeenh <furdeenh@udel.edu>
2024-04-12 21:09:44 -0400

    Expo Router Done 1

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, FlatList } from "react-native";
-import excercises from "./assets/data/exercises.json";
-import ExerciseListItem from "./src/components/ExcerciseListItem.jsx";
+import excercises from "../../assets/data/exercises.json";
+import ExerciseListItem from "../../src/components/ExcerciseListItem.jsx";
 
 
 
@@ -23,10 +23,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "gainsboro",
+    backgroundColor: "ghostwhite",
     justifyContent: "center",
     padding: 10,
-    paddingTop: 75,
   },
 
 });
--- src/components/ExcerciseListItem.jsx
+++ src/components/ExcerciseListItem.jsx
@@ -28,7 +28,20 @@ export default function ExerciseListItem({ item }) {
       padding: 10,
       borderRadius: 11,
       gap: 5,
-    },
+      marginHorizontal: 2,
+
+      //shadow dimensions
+
+      shadowColor: "#000",
+      shadowOffset: {
+	          width: 0,
+	          height: 1,
+      },
+      shadowOpacity: 0.20,
+      shadowRadius: 1.41,
+
+      elevation: 2,
+      },
 
 
     subValue: {
