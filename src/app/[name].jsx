b1c015d300decb846030b0805fd3d5816fa4230e
furdeenh <furdeenh@udel.edu>
2024-04-15 20:08:18 -0400

    See More and See Less Added

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -1,87 +1,77 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
-import { useLocalSearchParams } from 'expo-router'
-import exercises from '../../assets/data/exercises.json';
-import { Stack } from 'expo-router';
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import exercises from "../../assets/data/exercises.json";
+import { Stack } from "expo-router";
+import { useState } from "react";
 
 export default function ExcerciseDetailsScreen() {
-    const params = useLocalSearchParams();
+  const params = useLocalSearchParams();
 
+  const [isInstructionExpanded, setIsInstructionExpanded] = useState(false);
 
+  const exercise = exercises.find((item) => item.name == params.name);
 
-    const exercise = exercises.find((item) =>  item.name == params.name);
+  if (!exercise) {
+    return <Text>Exercise Not Found</Text>;
+  }
 
-    if (!exercise){
-        return <Text>Exercise Not Found</Text>;
-    }
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Stack.Screen options={{ title: exercise.name }} />
 
-    return(
-        <ScrollView contentContainerStyle={styles.container}>
-          <Stack.Screen options= {{ title: exercise.name }}/>
-
-
-          <View style={styles.panel}>
-
+      <View style={styles.panel}>
         <Text style={styles.exerciseName}>{exercise.name} </Text>
         <Text style={styles.exerciseSubtitle}>
-          <Text style={styles.subValue}>{exercise.muscle}</Text> |{' '} 
+          <Text style={styles.subValue}>{exercise.muscle}</Text> |{" "}
           <Text style={styles.subValue}>{exercise.equipment}</Text>
         </Text>
+      </View>
 
-        </View>
-
-
-        <View style={styles.panel}>
-
-
-
-        <Text style={styles.instructions}>
-          {exercise.instructions}
+      <View style={styles.panel}>
+        <Text style={styles.instructions} numberOfLines={isInstructionExpanded ? 0: 6}>{exercise.instructions}</Text>
+      <Text onPress={() => setIsInstructionExpanded(!isInstructionExpanded)} style={styles.seeMore}>
+        {isInstructionExpanded ? 'See Less' : 'See More'}
+        
         </Text>
-
-        </View>
-
-        </ScrollView>
-
-
-    );
-
+      </View>
+    </ScrollView>
+  );
 }
 
-
 const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    gap: 10,
+  },
 
-    container:{
-        padding: 10,
-        gap: 10,
-    },
+  panel: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+  },
 
-    panel:{
-      backgroundColor: 'white',
-      padding: 10,
-      borderRadius: 5,
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
 
-    },
+  exerciseSubtitle: {
+    color: "dimgray",
+  },
 
-    exerciseName: {
-        fontSize: 20,
-        fontWeight: "500",
-      },
-    
-      exerciseSubtitle: {
-        color: "dimgray",
-      },
+  subValue: {
+    textTransform: "uppercase",
+  },
 
-      subValue: {
-        textTransform: 'uppercase',
-      },
+  instructions: {
+    fontSize: 16,
+    lineHeight: 22,
+  },
 
-      instructions: {
-        fontSize: 16,
-        lineHeight: 22,
-
-      },
-
-
-
-
-})
+  seeMore: {
+    alignSelf: 'center',
+    padding: 5,
+    fontWeight: '600',
+    color: 'gray',
+  },
+});
